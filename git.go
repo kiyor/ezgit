05cf324f24294b5d99d9857711cc6691407764fa
kiyor <cyfkiyor@gmail.com>
2014-12-16 19:14:45 +0000

    better osexec

--- git.go
+++ git.go
@@ -6,7 +6,7 @@
 
 * Creation Date : 08-07-2014
 
-* Last Modified : Fri 08 Aug 2014 11:22:19 PM UTC
+* Last Modified : Tue 16 Dec 2014 07:14:22 PM UTC
 
 * Created By : Kiyor
 
@@ -16,9 +16,8 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 package ezgit
 
 import (
-	"errors"
 	"fmt"
-	"os/exec"
+	"github.com/kiyor/golib"
 	"strings"
 )
 
@@ -36,34 +35,25 @@ func NewGit(path string, bin string) *Git {
 	}
 }
 
-func osexec(cmd string) (stdOut string, stdErr error) {
-	out, stdErr := exec.Command("/bin/sh", "-c", cmd).Output()
-	stdOut = strings.TrimSpace(strings.Trim(string(out), "\n"))
-	if stdErr != nil {
-		stdErr = errors.New("[" + cmd + "]; " + stdErr.Error())
-	}
-	return
-}
-
 func (git *Git) Commit(comment string, files []string) error {
 	var fs string
 	for _, v := range files {
 		fs += v + " "
 	}
 	cmd := fmt.Sprintf("%s commit -m '%s' %s", git.prefix, comment, fs)
-	_, err := osexec(cmd)
+	_, err := golib.Osexec(cmd)
 	return err
 }
 
 func (git *Git) Push() error {
 	cmd := fmt.Sprintf("%s push", git.prefix)
-	_, err := osexec(cmd)
+	_, err := golib.Osexec(cmd)
 	return err
 }
 
 func (git *Git) PushTo(remote string) error {
 	cmd := fmt.Sprintf("%s push %s", git.prefix, remote)
-	_, err := osexec(cmd)
+	_, err := golib.Osexec(cmd)
 	return err
 }
 
@@ -73,7 +63,7 @@ func (git *Git) Add(files []string) error {
 		fs += v + " "
 	}
 	cmd := fmt.Sprintf("%s add %s", git.prefix, fs)
-	_, err := osexec(cmd)
+	_, err := golib.Osexec(cmd)
 	return err
 }
 
@@ -86,17 +76,17 @@ func (git *Git) Clone(remote string) error {
 		}
 	}
 	cmd := fmt.Sprintf("mkdir -p %s && cd %s && %s clone %s", path, path, git.bin, remote)
-	_, err := osexec(cmd)
+	_, err := golib.Osexec(cmd)
 	return err
 }
 
 func (git *Git) PullFile(branch string, file string) error {
 	cmd := fmt.Sprintf("%s fetch", git.prefix)
-	_, err := osexec(cmd)
+	_, err := golib.Osexec(cmd)
 	if err != nil {
 		return err
 	}
 	cmd = fmt.Sprintf("%s checkout %s -- %s", git.prefix, branch, file)
-	_, err = osexec(cmd)
+	_, err = golib.Osexec(cmd)
 	return err
 }
